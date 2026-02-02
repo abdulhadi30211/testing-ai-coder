22a9e7095a2f2089934a0c73074edc84824a0a51
Abdulhadi Web-Developer <aaaabbdulhadi573@gmail.com>
2026-02-02 18:53:57 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0vbhxp_xv-qA-EVBbxrX8bIZQEq9x0q0",
-  authDomain: "bolt-platform-clone.firebaseapp.com",
-  projectId: "bolt-platform-clone",
-  storageBucket: "bolt-platform-clone.firebasestorage.app",
-  messagingSenderId: "382922903531",
-  appId: "1:382922903531:web:5cdae922ce30481bc893ae",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
